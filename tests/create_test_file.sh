0926be3dacf2191dee0ed229d87fb014a11c43a6
fcying <fcyingmk2@gmail.com>
2024-08-30 18:53:19 +0800

    chore: update test build.log && test file

---
+++ tests/create_test_file.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+mkdir -p /opt/compiledb_test/src
+mkdir -p /opt/compiledb_test/src2
+
+cd /opt/compiledb_test/src
+
+touch ../fullpath.c
+
+touch test1.c
+touch test2.c
+touch test3.c
+touch test4.c
+
+touch test1.cc
+touch test1.cpp
+touch test2.cpp
